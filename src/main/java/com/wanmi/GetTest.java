b6024d9fa1390937c5a6612ef1ebd8334a7f2d3b
zhaishilin <1308282833@qq.com>
2022-06-01 11:07:45 +0800

    hot fix  commit second

--- src/main/java/com/wanmi/GetTest.java
+++ src/main/java/com/wanmi/GetTest.java
@@ -1,13 +1,10 @@
 package com.wanmi;
 
-/**
- * @Author: 作者
- * @Date: 2022/06/01/10:13
- * @Description: 致敬
- */
+
 public class GetTest {
     public static void main(String[] args) {
         System.out.println("hello github");
         System.out.println("hello github1");
+        System.out.println("hello github1  hot-fix");
     }
 }
