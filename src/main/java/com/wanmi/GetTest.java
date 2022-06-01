b5dfdfdfae02f73497de8cb741b16a06c675854d
zhaishilin <1308282833@qq.com>
2022-06-01 11:06:45 +0800

    master commit third

--- src/main/java/com/wanmi/GetTest.java
+++ src/main/java/com/wanmi/GetTest.java
@@ -9,5 +9,6 @@ public class GetTest {
     public static void main(String[] args) {
         System.out.println("hello github");
         System.out.println("hello github1");
+        System.out.println("hello github1");
     }
 }
