c79ee61f54d76c613048ee30e7fc0cc8863a375c
zhaishilin <1308282833@qq.com>
2022-06-01 10:20:05 +0800

    second commit

--- src/main/java/com/wanmi/GetTest.java
+++ src/main/java/com/wanmi/GetTest.java
@@ -7,6 +7,6 @@ package com.wanmi;
  */
 public class GetTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello github");
     }
 }
